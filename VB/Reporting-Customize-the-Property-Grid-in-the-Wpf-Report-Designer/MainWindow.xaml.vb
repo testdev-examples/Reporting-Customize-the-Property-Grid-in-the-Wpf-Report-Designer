ab1c11f5bfe22537faa4ded2841a2895776461f5
Andrey Lepikhov (auto) <andrey.lepikhov@gmail.com>
2019-05-17 11:04:43 +0300

    auto sync for version 2019.1

--- VB/Reporting-Customize-the-Property-Grid-in-the-Wpf-Report-Designer/MainWindow.xaml.vb
+++ VB/Reporting-Customize-the-Property-Grid-in-the-Wpf-Report-Designer/MainWindow.xaml.vb
@@ -7,22 +7,22 @@ Imports System.Linq
 Imports System.Windows
 
 Namespace CustomizePropertyGridCategories
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-            AddHandler XtraReport.FilterComponentProperties, AddressOf XtraReport_FilterComponentProperties
-            designer.OpenDocument(New XtraReport())
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+			AddHandler XtraReport.FilterComponentProperties, AddressOf XtraReport_FilterComponentProperties
+			designer.OpenDocument(New XtraReport())
+		End Sub
 
-        Private Sub XtraReport_FilterComponentProperties(ByVal sender As Object, ByVal e As FilterComponentPropertiesEventArgs)
-            Dim paperKindDescriptor = TryCast(e.Properties("PaperKind"), PropertyDescriptor)
-            If paperKindDescriptor IsNot Nothing Then
-                Dim attributes = New List(Of Attribute)(paperKindDescriptor.Attributes.Cast(Of Attribute)().Where(Function(att) Not (TypeOf att Is PropertyGridTabAttribute)))
-                attributes.Add(New PropertyGridTabAttribute("My tab"))
-                e.Properties("PaperKind") = TypeDescriptor.CreateProperty(paperKindDescriptor.ComponentType, paperKindDescriptor, attributes.ToArray())
-            End If
-        End Sub
-    End Class
+		Private Sub XtraReport_FilterComponentProperties(ByVal sender As Object, ByVal e As FilterComponentPropertiesEventArgs)
+			Dim paperKindDescriptor = TryCast(e.Properties("PaperKind"), PropertyDescriptor)
+			If paperKindDescriptor IsNot Nothing Then
+				Dim attributes = New List(Of Attribute)(paperKindDescriptor.Attributes.Cast(Of Attribute)().Where(Function(att) Not (TypeOf att Is PropertyGridTabAttribute)))
+				attributes.Add(New PropertyGridTabAttribute("My tab"))
+				e.Properties("PaperKind") = TypeDescriptor.CreateProperty(paperKindDescriptor.ComponentType, paperKindDescriptor, attributes.ToArray())
+			End If
+		End Sub
+	End Class
 End Namespace
